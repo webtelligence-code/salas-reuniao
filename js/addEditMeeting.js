55941c623e754890be7a382b0b534cffb9fdfd71
webtelligence-code <manuelbcarreiras@gmail.com>
2023-04-05 12:59:11 +0100

    Added some form logic for date and hora_inicio field

--- js/addEditMeeting.js
+++ js/addEditMeeting.js
@@ -7,8 +7,23 @@ const addEditMeetingbtn = document.getElementById('add-edit-meeting-btn');
 // Empty meeting to handle new or updated meeting
 const meeting = {};
 
-// Event listener for form submit
-meetingForm.addEventListener('submit', (e) => submitMeeting(e));
+const setMinimumDate = () => {
+    const dateInput = document.getElementById('data');
+
+    const today = new Date();
+    const minDate = today.toLocaleDateString('en-CA', { timeZone: 'Europe/Lisbon', year: 'numeric', month: '2-digit', day: '2-digit' });
+
+    dateInput.setAttribute('min', minDate);
+}
+setMinimumDate();
+
+const roundToNearestHalfHour = (date) => {
+    const minutes = date.getMinutes();
+    const roundedMinutes = (minutes < 30 ? 0 : 30);
+    date.setMinutes(roundedMinutes);
+    date.setSeconds(0);
+    return date;
+}
 
 // Fucntion that will handle labels for the title and button based on action (add/edit)
 const handleDOMLabels = () => {
@@ -28,7 +43,7 @@ const handleFormData = () => {
     getRooms();
     if (selectedMeeting) {
         document.getElementById('motivo').value = selectedMeeting.motivo;
-        document.getElementById('data').value = selectedMeeting.data;
+        //document.getElementById('data').value = selectedMeeting.data;
         document.getElementById('hora_inicio').value = selectedMeeting.hora_inicio;
         document.getElementById('hora_fim').value = selectedMeeting.hora_fim;
     }
@@ -40,6 +55,8 @@ const getRooms = () => {
         if (status === 'success') {
             const parsedData = JSON.parse(data)
             populateRooms(parsedData); // Populate Rooms Select dropdown
+        } else {
+            console.log('Failed to fetch rooms from api.');
         }
     })
 }
@@ -88,6 +105,18 @@ const addMeeting = () => {
 }
 
 handleDOMLabels(); // Call handleDOMLabels
+
+// Event listener for form submit
+meetingForm.addEventListener('submit', (e) => submitMeeting(e));
+
+// Set the initial value of the time input to the nearest half-hour
+document.addEventListener('DOMContentLoaded', () => {
+    const horaInicioInput = document.getElementById('hora_inicio');
+    const now = new Date();
+    const nearestHalfHour = roundToNearestHalfHour(now);
+    const timeString = nearestHalfHour.toISOString().substr(11, 5);
+    horaInicioInput.value = timeString;
+});
 
 // Log the selected meeting object (if present)
 console.log('Selected Meeting:', selectedMeeting);
